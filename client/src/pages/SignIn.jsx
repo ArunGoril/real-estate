ab749464e4b99f7330715cdfb69a779a94b373b9
ArunKumarGoril <arun.kumar25b5af@cognizant.com>
2024-04-28 21:59:47 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice'
 
 const SignIn = () => {
   const [formData, setFromData] = useState({})
-  const [error, setError] = useState(null)
-  const [isLoading, setIsLoading] = useState(false)
+  const {loading, error} = useSelector(state => state.user)
+
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const handleChange = (e) => {
     setFromData({ ...formData, [e.target.id]: e.target.value })
@@ -15,7 +18,7 @@ const SignIn = () => {
     e.preventDefault()
 
     try {
-      setIsLoading(true)
+      dispatch(signInStart())
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -26,16 +29,13 @@ const SignIn = () => {
       const data = await res.json()
       // console.log(data)
       if (data.success === false) {
-        setError(data.message)
-        setIsLoading(false)
+        dispatch(signInFailure(data.message))
         return
       }
-      setIsLoading(false)
-      setError(null)
+      dispatch(signInSuccess(data))
       navigate('/')
     } catch (error) {
-      setIsLoading(false)
-      setError(error.message)
+      dispatch(signInFailure(data.message))
     }
   }
 
@@ -45,8 +45,8 @@ const SignIn = () => {
       <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
         <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
         <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
-        <button type='submit' disabled={isLoading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-          {isLoading ? 'Loading...' : 'Sign In'}
+        <button type='submit' disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          {loading ? 'Loading...' : 'Sign In'}
         </button>
       </form>
       <div className='flex gap-2 mt-5'>
